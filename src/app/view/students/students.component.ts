93984bb0ad25bbbb025bcab067add5893c8e99d8
Gerasimov Dmitry <gerasimov-dmr@yandex.ru>
2019-12-03 14:07:18 +0300

    Добавлен поиск студента по имени и(или) фамилии

--- src/app/view/students/students.component.ts
+++ src/app/view/students/students.component.ts
@@ -11,6 +11,8 @@ export class StudentsComponent implements OnInit {
 
   students: Student[];
   feature: boolean = true;
+  search: string = "";
+  searchField: string = "All";
 
   constructor(private dataHandler: DataHandlerService) { }
 
@@ -24,5 +26,34 @@ export class StudentsComponent implements OnInit {
 
   getFeature(): boolean {
     return this.feature;
+  }
+
+  isSearch(student: Student, searchField: string): boolean {
+    if (!this.search.trim()) {
+      return false;
+    }
+    switch (searchField) {
+      case "All":
+        if (student.lastName.toLocaleLowerCase().includes(this.search.toLowerCase()) ||
+        student.firstName.toLocaleLowerCase().includes(this.search.toLowerCase())) {
+          return true;
+        }
+        break;
+      case "lastName":
+        if (student.lastName.toLocaleLowerCase().includes(this.search.toLowerCase())) {
+          return true;
+        }
+        break;
+      case "firstName":
+        if (student.firstName.toLocaleLowerCase().includes(this.search.toLowerCase())) {
+          return true;
+        }
+        break;
+    }
+    // if (student.lastName.toLocaleLowerCase().includes(this.search.toLowerCase()) ||
+    //   student.firstName.toLocaleLowerCase().includes(this.search.toLowerCase())) {
+    //   return true;
+    // }
+    return false;
   }
 }
